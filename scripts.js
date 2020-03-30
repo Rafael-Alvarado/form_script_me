f64d6ce30213a257cac4c970fc08825f472506fb
Rafael-Alvarado <rafaelalvarado286@gmail.com>
2020-03-30 15:05:16 -0430

    Otro comit

--- scripts.js
+++ scripts.js
@@ -1,39 +1,52 @@
 function validar(){
+
     var nombre, id, cantidad, fecha;
+
     var expresion;
+
     nombre = document.getElementById("nombreMedicina").value;
+
     id = document.getElementById("idMedicina").value;
+
     cantidad = document.getElementById("cantidadMedicina").value;
+
     fecha = document.getElementById("fechaMedicina").value;
+
     if(nombre == null || nombre.length == 0){
         alert("Todos los campos son obligatorios");
         return false;
     }
+
     if(/^\s+$/.test(nombre)){
         alert("No puedes enviar espacios");
         return false;
     }
-    if(nombre.length<4)
+
     if(id == null || id.length == 0){
         alert("Nombre no valido");
         return false;
     }
+
     if(/^\s+$/.test(id)){
         alert("No puedes enviar espacios");
         return false;
     }
+
     if(cantidad == null || cantidad.length == 0){
         alert("Todos los campos son obligatorios");
         return false;
     }
+
     if(/^\s+$/.test(cantidad)){
         alert("No puedes enviar espacios");
         return false;
     }
+
     if(fecha == null || fecha.length == 0){
         alert("Todos los campos son obligatorios");
         return false;
     }
+    
     if(/^\s+$/.test(fecha)){
         alert("No puedes enviar espacios");
         return false;
